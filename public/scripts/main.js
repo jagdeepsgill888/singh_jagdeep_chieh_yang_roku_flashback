ebec07dd948d327c4e9b9fbfdf1f69ca2bb22479
jagdeepsgill888 <59809683+jagdeepsgill888@users.noreply.github.com>
2021-04-13 21:05:15 -0400

    before adding vidoes

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,27 +1,62 @@
-import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.12/dist/vue.esm.browser.js'
-import TheMovieThumbnail from './components/TheMovieThumbnailComponent.js';
+import LoginComponent from './components/TheLoginComponent.js';
+import AllUsers from './components/TheAllUsersComponents.js';
+import HomeComponent from './components/TheHomeComponent.js';
+
+const router = new VueRouter({
+    routes: [
+        { path: '/', name: 'root', component: LoginComponent, beforeEnter: (to, from, next) =>{
+            //if you're authenticated (set in localstorage), then go to the home page
+            if (localStorage.getItem('cacheduser')) {
+                let user = JSON.parse(localStorage.getItem('cacheduser'));
+                next({name: 'home', params: {currentuser: user}});
+            } else {
+                next();
+            }
+        } },
+        { path: '/users', name: 'users', component: AllUsers },
+        { path: '/home', name: 'home', component: HomeComponent, props: true }
+    ]
+});
+
 (() =>{
     const vm = new Vue({
         data: {
-            allMovies: []
+            allMovies: [],
+            authenticated: false,
+            isAdmin: false,
+            currentUser: undefined
         },
 
         created: function() {
-            fetch('/api/movies')
-              .then(res => res.json())
-              .then(data => {
-                // show the data in table form
-                console.table(data);
-                this.allMovies = data;
-            })
-            .catch(err => console.error(err));
+            // fetch('/api/movies')
+            //   .then(res => res.json())
+            //   .then(data => {
+            //     // show the data in table form
+            //     console.table(data);
+            //     this.allMovies = data;
+            // })
+            // .catch(err => console.error(err));
         },
 
         methods: {
+            logout() {
+                //remove the cached user from localSorage, if it exists
+                if(localStorage.getItem('cacheduser')){
+                    localStorage.removeItem('cacheduser')
+                }
+             this.$router.push({ name: "root"});
+             this.currentUser = undefined;
+         },
 
-        },
-        components: {
-            moviethumb: TheMovieThumbnail
+         authenticateuser(user) {
+            this.currentUser = user;
         }
+        },
+
+        components: {
+            // moviethumb: TheMovieThumbnail
+        },
+
+        router
     }).$mount("#app");
 })();
